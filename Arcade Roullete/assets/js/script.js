f30cfc4ecfb6c9b960cb82709e08a357f3b4ca22
joemardev <joemarb.dev@gmail.com>
2022-09-22 16:42:09 +0800

    Update game

--- Arcade Roullete/assets/js/script.js
+++ Arcade Roullete/assets/js/script.js
@@ -1,3 +1,10 @@
+const URL = "http://127.0.0.1:8000/api";
+
+let history = [];
+
+const Pos = [720, 765, 810, 855, 900, 945, 990, 1035];
+
+
 const Toogle = (elem) => {
     return $(elem).hasClass('on') ? $(elem).removeClass('on') : $(elem).addClass('on');
 }
@@ -16,20 +23,19 @@ const RunGameTimeAndRound = (city, offset) => {
         const GameHour = nd.getHours();
         const GameMinute = nd.getMinutes();
 
-        const GameRound = (GameHour * 60) + GameMinute;
+        const GameRound = ((GameHour * 60) + GameMinute) + 1;
         // get korean timezone seconds
         const GameSec = (60 - nd.getSeconds()).toString().padStart(2, '0');
-
+        
         if (GameSec == 60) {
-            RunAnimation(0, 4);
+            RunAnimation(0, 1);
         }
 
-        $('#roullete .round').html(GameRound + 1);
+        $('#roullete .round').html(GameRound  );
         $('#roullete .time').html(GameSec == 60 ? 0 : GameSec);
     }, 1000);
 };
 
-const Pos = [720, 765, 810, 855, 900, 945, 990, 1035];
 
 
 const RunAnimation = (res, distance) => {
@@ -73,7 +79,54 @@ const RunAnimation = (res, distance) => {
         }, 500);
 
     }, 100);
-
 }
 
-RunGameTimeAndRound('korea', '+9');
+const RefreshHistory = () => {
+    $.ajax(URL + '/arcade-routellete/history').then((res) => {
+        res.map((item) => {
+            populateResult(item);
+        })
+    })
+}
+
+const populateResult = (res) => {
+    let res_name_1 = '';
+    let res_icon_2 = '';
+
+    res.result % 2 == 0 ? res_icon_2 = 'icon_result_1' : res_icon_2 = 'icon_result_2';
+
+    if( res.result % 4 == 0) res_name_1 = 'icon_sinsu_3';
+    if( res.result % 4 == 1) res_name_1 = 'icon_sinsu_1';
+    if( res.result % 4 == 2) res_name_1 = 'icon_sinsu_2';
+    if( res.result % 4 == 3) res_name_1 = 'icon_sinsu_4';
+
+
+    let date = new Date(res.created_at);
+    let options = {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+        timeZone: "Asia/Seoul"
+    };
+    let KoreaFormat = date.toLocaleString("ko-KR", options);
+    KoreaFormat = KoreaFormat.substring(0 , KoreaFormat.length - 1);
+
+    $('.history').prepend(
+        '<div class="listitem">'+
+            `<span class="date">${KoreaFormat}</span>`+
+            '<span>&ndash;</span>'+
+            `<span class="round"><strong>${res.round}</strong>회차</span>`+
+            `<span class="routelle-image icon_sinsu ${res_name_1}"></span>`+
+            `<span class="routelle-image icon_result ${res_icon_2}"></span>`+
+        '</div>'
+    )
+}
+
+const InitGame = () => {
+    RefreshHistory();
+    RunGameTimeAndRound('korea', '+9');
+}
+
+
+
+InitGame();
