80038035d4085ca0bc2c7bc252070b7dabd18807
joemardev <joemarb.dev@gmail.com>
2022-09-23 13:29:57 +0800

    Add Result Bar Connect in Online Server

--- Arcade Roullete/assets/js/script.js
+++ Arcade Roullete/assets/js/script.js
@@ -1,8 +1,14 @@
-const URL = "http://127.0.0.1:8000/api";
+const URL = "https://realbet-server.online/api";
+
+const backgroundMusic = new Audio('./assets/sounds/bing_bg.mp3');
+const resultMusic = new Audio('./assets/sounds/bing_play.mp3');
+
+let isBrowserSoundReady = false;
+let globalSeconds = 0;
 
 let history = [];
-
-const Pos = [720, 765, 810, 855, 900, 945, 990, 1035];
+        //     1     4     4     3     3     2     2     1
+const Pos = [3960, 4005, 4050, 4095, 4140, 4185, 4230, 4275];
 
 
 const Toogle = (elem) => {
@@ -22,20 +28,65 @@ const RunGameTimeAndRound = (city, offset) => {
 
         const GameHour = nd.getHours();
         const GameMinute = nd.getMinutes();
-
+        
         const GameRound = ((GameHour * 60) + GameMinute) + 1;
         // get korean timezone seconds
         const GameSec = (60 - nd.getSeconds()).toString().padStart(2, '0');
         
-        if (GameSec == 60) {
-            RunAnimation(0, 1);
+        if (GameSec == 60)  {
+            backgroundMusic.pause();
+            resultMusic.play();
+            GetResult();
         }
-
-        $('#roullete .round').html(GameRound  );
+   
+        $('#roullete .round').html(GameRound);
         $('#roullete .time').html(GameSec == 60 ? 0 : GameSec);
     }, 1000);
 };
 
+const InitSounds = async () => {
+    isBrowserSoundReady = true;
+    await CheckConfigSaved();
+    backgroundMusic.loop = true;
+    backgroundMusic.play();
+}
+
+const MusicToogle = (elem) => {
+    localStorage.setItem("sounds", (!$(elem).hasClass('on')));
+    backgroundMusic.muted = $(elem).hasClass('on');
+};
+
+const SoundEffectToogle = (elem)=> {
+    localStorage.setItem("effect", (!$(elem).hasClass('on')));
+    resultMusic.muted = $(elem).hasClass('on');
+};
+
+const CheckConfigSaved = () => {
+    let soundConfig = localStorage.getItem("sounds");
+    let effectConfig = localStorage.getItem("effect");
+
+    if(soundConfig == 'false') {
+        backgroundMusic.muted = true;
+        $('.music').addClass('on');
+    }
+
+    if(effectConfig == 'false') {
+        resultMusic.muted = true;
+        $('.sound').addClass('on');
+    }
+}
+
+
+
+const GetResult = async() =>{
+    $.ajax(URL + '/arcade-routellete').then((res) => {
+        RunAnimation((res.result - 1),1);
+        setTimeout(() => {
+            populateResult(res,'result');
+            backgroundMusic.play();
+        },8200);
+    });
+}
 
 
 const RunAnimation = (res, distance) => {
@@ -46,7 +97,7 @@ const RunAnimation = (res, distance) => {
 
     const ticker = Math.floor(Math.random() * 20) * plusOrMinus;
 
-    let resPosition = Pos[rand] * distance;
+    let resPosition = Pos[res] * distance;
 
     resPosition += ticker;
 
@@ -81,24 +132,24 @@ const RunAnimation = (res, distance) => {
     }, 100);
 }
 
-const RefreshHistory = () => {
+const RefreshHistory =  () => {
     $.ajax(URL + '/arcade-routellete/history').then((res) => {
         res.map((item) => {
-            populateResult(item);
-        })
+            populateResult(item , 'history');
+        });
     })
 }
 
-const populateResult = (res) => {
+const populateResult = (res , type) => {
     let res_name_1 = '';
     let res_icon_2 = '';
 
     res.result % 2 == 0 ? res_icon_2 = 'icon_result_1' : res_icon_2 = 'icon_result_2';
 
-    if( res.result % 4 == 0) res_name_1 = 'icon_sinsu_3';
-    if( res.result % 4 == 1) res_name_1 = 'icon_sinsu_1';
-    if( res.result % 4 == 2) res_name_1 = 'icon_sinsu_2';
-    if( res.result % 4 == 3) res_name_1 = 'icon_sinsu_4';
+    if( res.result  == 1 || res.result  == 8) res_name_1 = 'icon_sinsu_1';
+    if( res.result  == 6 || res.result  == 7) res_name_1 = 'icon_sinsu_2';
+    if( res.result  == 4 || res.result  == 5) res_name_1 = 'icon_sinsu_3';
+    if( res.result  == 2 || res.result  == 3) res_name_1 = 'icon_sinsu_4';
 
 
     let date = new Date(res.created_at);
@@ -110,22 +161,59 @@ const populateResult = (res) => {
     };
     let KoreaFormat = date.toLocaleString("ko-KR", options);
     KoreaFormat = KoreaFormat.substring(0 , KoreaFormat.length - 1);
+    
+    if(type == 'history') {
+        $('.history').prepend(ListResultBody(KoreaFormat,res,res_name_1,res_icon_2));
+    }
 
-    $('.history').prepend(
-        '<div class="listitem">'+
-            `<span class="date">${KoreaFormat}</span>`+
-            '<span>&ndash;</span>'+
-            `<span class="round"><strong>${res.round}</strong>회차</span>`+
-            `<span class="routelle-image icon_sinsu ${res_name_1}"></span>`+
-            `<span class="routelle-image icon_result ${res_icon_2}"></span>`+
-        '</div>'
-    )
+    if(type == 'result') {
+        $('.history').prepend(ListResultBody(KoreaFormat,res,res_name_1,res_icon_2));
+        $('.result_box').show();
+        $('.result_box').html(GameResultBody(res,res_name_1,res_icon_2));
+        ResetResultBox();
+    }
+   
 }
+
+const ListResultBody = (KoreaFormat,res,res_name_1,res_icon_2) => {
+    return '<div class="listitem">'+
+                `<span class="date">${KoreaFormat}</span>`+
+                '<span>&ndash;</span>'+
+                `<span class="round"><strong>${res.round}</strong>회차</span>`+
+                `<span class="routelle-image icon_sinsu ${res_name_1}"></span>`+
+                `<span class="routelle-image icon_result ${res_icon_2}"></span>`+
+            '</div>';
+}
+
+const GameResultBody = (res,res_name_1,res_icon_2) => {
+    return  `<div class="content_box">`+
+                `<span class="res-round">${res.round}회차 결과</span>`+
+                `<span class="routelle-image result_icon  ${res_name_1}"></span>`+
+                `<span class="routelle-image result_icon  ${res_icon_2}"></span>`+
+           ` </div>`;
+}
+
+const ResetResultBox = () => {
+    setTimeout(() => {
+        $('.result_box').hide();
+        $('.result_box').html();
+    },5000);
+}
+
 
 const InitGame = () => {
     RefreshHistory();
     RunGameTimeAndRound('korea', '+9');
 }
+
+const inIframe = () => {
+    try {
+        return window.self !== window.top;
+    } catch (error) {
+        return true;
+    }
+}
+
 
 
 
